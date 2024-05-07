7d46a314948e77ba76d5e86a4540b2b797f41aaa
Julia-slvbrg <julia.slvb@gmail.com>
2024-05-07 19:05:59 -0300

    chore: components implemented

---
+++ src/components/PForm.vue
@@ -0,0 +1,24 @@
+<script setup>
+    const props = defineProps(['submitTask', 'tempTask', 'changeFilter', 'editTempTask'])
+</script>
+
+<template>
+    <form @submit.prevent="props.submitTask">
+    <div class="row">
+      <div class="col">
+        <input :value="props.tempTask" @change="props.editTempTask" required type="text" placeholder="Descrição da tarefa" class="form-control">
+      </div>
+      <div class="col-md-1">
+        <button type="submit" class="btn btn-primary btn-md">Cadastrar</button>
+      </div>
+    </div>
+
+    <div class="col-md-2">
+      <select @change="props.changeFilter" class="form-control">
+        <option value="all">Todas as tarefas</option>
+        <option value="to-do">Pendentes</option>
+        <option value="done">Finalizadas</option>
+      </select>
+    </div>
+  </form>
+</template>
